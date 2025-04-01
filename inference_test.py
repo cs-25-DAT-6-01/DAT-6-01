cb60f2059ed1af19cae5036848711d1a7acc0155
KanzanKohi <agredder@gmail.com>
2025-04-01 15:53:03 +0200

    changed parameters

--- inference_test.py
+++ inference_test.py
@@ -44,11 +44,10 @@ output = model.generate(
     attention_mask=attention_mask,
     max_new_tokens=100,
     #temperature=0.7,
-    #top_k=50,
+    top_k=50,
     #top_p=0.9,
     repetition_penalty=1.2,
     do_sample=False,
-    eos_token_id=None
 )
 
 # Decode the output
