36884e5967ee34b684bc20f6a8ebc67ac349ee3b
KanzanKohi <agredder@gmail.com>
2025-04-08 23:46:14 +0200

    possible fixes

--- fine_tuning.py
+++ fine_tuning.py
@@ -27,7 +27,7 @@ bnb_config = BitsAndBytesConfig(
 model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True, quantization_config=bnb_config)
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
 tokenizer.pad_token = tokenizer.eos_token
-#tokenizer.pad_token_id = tokenizer.eos_token_id
+tokenizer.pad_token_id = tokenizer.eos_token_id
 
 # Set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
