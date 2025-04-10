d36e16f5519b95459046a629ae95a259d65e4dee
Alexander Redder <109695232+KanzanKohi@users.noreply.github.com>
2025-04-10 12:51:31 +0200

    testing with normal gpt to see laers

--- inference_test.py
+++ inference_test.py
@@ -15,7 +15,7 @@ tokenizer_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2"
 
 # Load the model and tokenizer
 base_model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
-model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
 #model = PeftModel.from_pretrained(base_model, model_path)
 #model = model.merge_and_unload()
 
