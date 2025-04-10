00813d246e6bcb99af53d4e70c768a9944b50622
Alexander Redder <109695232+KanzanKohi@users.noreply.github.com>
2025-04-10 12:45:41 +0200

    test in order to see model layers

--- inference_test.py
+++ inference_test.py
@@ -15,9 +15,9 @@ tokenizer_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2-
 
 # Load the model and tokenizer
 base_model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
-#model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
-model = PeftModel.from_pretrained(base_model, model_path)
-model = model.merge_and_unload()
+model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
+#model = PeftModel.from_pretrained(base_model, model_path)
+#model = model.merge_and_unload()
 
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
 tokenizer.pad_token = tokenizer.eos_token
