70f4e66219169874d344c3d9ba373ce45f217834
KanzanKohi <agredder@gmail.com>
2025-04-16 18:57:00 +0200

    fixes

--- inference_test.py
+++ inference_test.py
@@ -10,15 +10,15 @@ model_name = "openai-community-gpt2"
 amount_of_epochs = "6"
 
 # Path to the trained model/tokenizer
-base_model_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2"
-model_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2-fine_tuning-1"
-tokenizer_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2-fine_tuning-1"
+# = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-1.2"
+model_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-0.7"
+tokenizer_path = f"model-{model_name}_epochs-{amount_of_epochs}_temperature-0.7"
 
 # Load the model and tokenizer
-base_model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
-#model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
-model = PeftModel.from_pretrained(base_model, model_path)
-model = model.merge_and_unload()
+#base_model = AutoModelForCausalLM.from_pretrained(base_model_path, local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True)
+#model = PeftModel.from_pretrained(base_model, model_path)
+#model = model.merge_and_unload()
 
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
 tokenizer.pad_token = tokenizer.eos_token
