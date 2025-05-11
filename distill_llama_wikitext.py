ec5c2703f701fdcc5043853bde0a61800d8dd6be
KanzanKohi <agredder@gmail.com>
2025-05-11 17:33:16 +0200

    changed max_length to 128

--- distill_llama_wikitext.py
+++ distill_llama_wikitext.py
@@ -21,7 +21,7 @@ def new_distillation_loss(alpha, beta,  student, teacher, tokenizer, embedder, g
                 )
 
         teacher_texts = [tokenizer.decode(out, skip_special_tokens=True) for out in teacher_outputs]
-        teacher_inputs = tokenizer(teacher_texts, return_tensors="pt", padding=True, truncation=True, max_length=512).to(student_first_device)
+        teacher_inputs = tokenizer(teacher_texts, return_tensors="pt", padding=True, truncation=True, max_length=128).to(student_first_device)
         student_logits = student(teacher_inputs.input_ids, attention_mask=teacher_inputs.attention_mask).logits
         
         shift_logits = student_logits[..., :-1, :].contiguous()
@@ -45,7 +45,7 @@ def new_distillation_loss(alpha, beta,  student, teacher, tokenizer, embedder, g
         loss_embed = F.mse_loss(student_embeddings.to(student_first_device), teacher_embeddings.to(student_first_device))
         
         # Consistency CE
-        student_free_inputs = tokenizer(student_texts, return_tensors="pt", padding=True, truncation=True, max_length=512).to(student_first_device)
+        student_free_inputs = tokenizer(student_texts, return_tensors="pt", padding=True, truncation=True, max_length=128).to(student_first_device)
         free_logits = student(student_free_inputs.input_ids, attention_mask=student_free_inputs.attention_mask).logits
         shift_free_logits = free_logits[..., :-1, :].contiguous()
         shift_teacher_labels = teacher_inputs.input_ids[..., 1:].contiguous()
@@ -125,7 +125,7 @@ def train():
      # Tokenize the dataset
     def tokenize_function(examples):
         return teacher_tokenizer(examples['text'], return_tensors="pt", padding="max_length", truncation=True,
-                                 max_length=512)
+                                 max_length=128)
 
     print("Starting tokenization")
     train_dataset = train_dataset.map(tokenize_function, batched=True)
@@ -134,7 +134,7 @@ def train():
     train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask'])
     #test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask'])
 
-    train_dataloader = DataLoader(train_dataset, batch_size=1, num_workers=2, pin_memory=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=4, num_workers=2, pin_memory=True)
     # test_dataloader = DataLoader(test_dataset, batch_size=16, num_workers=4, pin_memory=True)
     
     student_first_device = list(student_model.hf_device_map.values())[0]
