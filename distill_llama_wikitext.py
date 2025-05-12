3d005bd9c4349cde28c237549184740deda7b927
KanzanKohi <agredder@gmail.com>
2025-05-12 11:58:54 +0200

    new params

--- distill_llama_wikitext.py
+++ distill_llama_wikitext.py
@@ -208,11 +208,11 @@ def train():
     ppl_history = []
 
     for epoch in range(num_epochs):
-        alpha=6.0
-        lambd=0.5
+        alpha=10
+        lambd=0.2
         beta=0.5
         gamma=1.0
-        temperature=3.0
+        temperature=1.5
         student_model.train()
 
         total_loss = 0
