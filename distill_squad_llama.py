e43710ae20d3bcb4b1355fc9c56c3a2fab3098e9
KanzanKohi <agredder@gmail.com>
2025-05-16 13:14:38 +0200

    params

--- distill_squad_llama.py
+++ distill_squad_llama.py
@@ -142,11 +142,11 @@ def train():
 
     print("Starting training")
     for epoch in range(num_epochs):
-        alpha = 6
-        lambd = 0.5
+        alpha = 10
+        lambd = 0.2
         beta = 0.5
         gamma = 1
-        temperature = 3
+        temperature = 1.5
         student_model.train()
 
         total_loss = 0
